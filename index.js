cb67389df3b7e26d6bfe57f2bb218305e6708a6f
Dawid <dawid.debo2.pl@gmail.com>
2019-03-28 19:31:17 +0100

    Commit działającej wersji :D

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 function onLoad() {
+    const display = document.getElementById("display");
+    let state = "0";
+    const render = () => display.innerHTML =  state;
+    const handle = (key) => {
+        if(!isNaN(Number(state + key))){
+            state += key;  
+        }
+        else if(["C", "c"].includes(key)){
+            state = "0";
+        }
+        if(state!== "0" && state[0] === "0" && ![".", ","].includes(state[1])){
+            state = state.slice(1);
+        }
+        render();
+    }
+    const handleMouse = (event) => {
+        handle(event.target.innerHTML)
+    }
+    const handleKeyboard = (event) => {
+        handle(event.key)
+    }
+    window.addEventListener("keydown", handleKeyboard)
+    document.querySelectorAll(".key")
+        .forEach(key => key.addEventListener("click", handleMouse));
+
 }
 
 window.addEventListener("load", onLoad);
