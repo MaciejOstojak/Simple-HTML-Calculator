7d9f11c2f30e21ce91646cbef05e4e26dab0f2a8
AndrzejWojciechowski0 <wojciechowski969@gmail.com>
2019-04-04 19:53:38 +0200

    zrobiony kalkulator

--- index.js
+++ index.js
@@ -1,20 +1,63 @@
 function onLoad() {
     const display = document.getElementById("display");
-    let state = "0";
-    const render = () => display.innerHTML =  state;
+    let state = "0"; // pierwsza z liczb
+    let operation = ""; // operacja matematyczna do wykonania
+    let operanda = ""; // druga z wybranych liczb
+    const operations = new Map();
+    
+
+    operations.set("+",()=>state = (Number(state) + Number(operanda)).toString())
+    operations.set("-",()=>state = (Number(state) - Number(operanda)).toString())
+    operations.set("*",()=>state = (Number(state) * Number(operanda)).toString())
+    operations.set("/",()=>state = (Number(state) / Number(operanda)).toString())
+    const calculate = () => {
+        operations.get(operation)();
+            operation = "";
+            operanda = "";
+    }
+
+    const render = () => {
+        if (operanda !=="") display.innerHTML = operanda;
+       else if(operation !=="") display.innerHTML = operation;
+    else display.innerHTML = state;
+    }
+
     const handle = (key) => {
-        if(!isNaN(Number(state + key))){
+        if (operation === "" && !isNaN(Number(state + key))) {
             state += key;  
         }
+        else if(operation !== "" && !isNaN(Number(operanda + key))) {
+            operanda += key;
+        }
+    
         else if(["C", "c"].includes(key)){
             state = "0";
+            operation ="";
+            operanda ="";
         }
         else if("Backspace" === key && !isNaN(Number(state.slice(0, -1)))){
             state = state.slice(0, -1)
+            if(state==="") state="0";
         }
+        else if (["-", "+","*","/"].includes(key)){
+            if(operanda !== "")
+                 calculate();
+            operation = key;
+
+            
+
+        }
+         else if (["=", "Enter"].includes(key)){
+             operations.get(operation)();
+             operation = "";
+             operanda = "";
+  
+         }
+        
         if(state!== "0" && state[0] === "0" && ![".", ","].includes(state[1])){
             state = state.slice(1);
         }
+
         render();
     }
     const handleMouse = (event) => {
